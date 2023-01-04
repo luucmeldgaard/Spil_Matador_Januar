a42f8f94e2cc840aaa12d50477e4afb8fa6711c6
Anton <s224268@student.dtu.dk>
2023-01-04 18:27:56 +0100

    Created constructor for Property, assigning values

--- src/main/java/dtu/matador/game/Property.java
+++ src/main/java/dtu/matador/game/Property.java
@@ -11,6 +11,19 @@ public abstract class Property implements Properties {
     String owner;
     boolean purchaseable = false;
     int price;
+
+    //Takes input to create the class
+    public Property(String name, String subtext, String description, String rent,
+                    String color_1,String color_2, String price, String owner){
+        this.propertyName = name;
+        this.subtext = subtext;
+        this.description = description;
+        this.rent = Integer.parseInt(rent);
+        this.price = Integer.parseInt(price);
+        this.color_1 = color_1;
+        this.color_2 = color_2;
+        this.owner = owner;
+    }
 }
 
 
