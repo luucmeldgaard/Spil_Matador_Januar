fc0d0b016cafa1935845e4a772a2aa456ba53b38
Haidar Aqil Alwandi <Haidar2001@hotmail.dk>
2023-01-04 18:24:05 +0100

    Terning implementeret

--- src/main/java/dtu/matador/game/Die.java
+++ src/main/java/dtu/matador/game/Die.java
@@ -1,5 +1,14 @@
 package dtu.matador.game;
+import java.util.Random;
 
 public class Die {
+
+    public Die(){}
+
+//Each dice will show a number between 1 and 6.
+    public int roll(int sides) {
+        Random random = new Random();
+        return random.nextInt(0,6)+1;
+    }
 
 }
