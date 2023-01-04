e685eb23852be3e7b7b905dc9aaa23ac7eb0bbd0
Haidar Aqil Alwandi <Haidar2001@hotmail.dk>
2023-01-04 18:44:22 +0100

    DiceCup implementeret

--- src/main/java/dtu/matador/game/DiceCup.java
+++ src/main/java/dtu/matador/game/DiceCup.java
@@ -1,5 +1,10 @@
 package dtu.matador.game;
 
 public class DiceCup {
+    Die die = new Die();
+    int sides = 6;
+    public int roll() {
+        return die.roll(sides) + die.roll(sides);
+    }
 
 }
