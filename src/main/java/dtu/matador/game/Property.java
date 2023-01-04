24bd0bca4aceb63c5e6d2d069e11551079b3ddc3
Anton <s224268@student.dtu.dk>
2023-01-04 18:23:18 +0100

    Created variables that all properties have in common

--- src/main/java/dtu/matador/game/Property.java
+++ src/main/java/dtu/matador/game/Property.java
@@ -1,5 +1,18 @@
 package dtu.matador.game;
 
 public abstract class Property implements Properties {
-
+    //Creating variables that will be used
+    String propertyName;
+    String subtext;
+    String description;
+    int rent;
+    String color_1;
+    String color_2;
+    String owner;
+    boolean purchaseable = false;
+    int price;
 }
+
+
+
+
