38b2eb767b9f2e99e6c48bc082c853214e394ba9
Haidar Aqil Alwandi <Haidar2001@hotmail.dk>
2023-01-04 19:05:37 +0100

    Player implementeret

--- src/main/java/dtu/matador/game/Player.java
+++ src/main/java/dtu/matador/game/Player.java
@@ -2,4 +2,39 @@ package dtu.matador.game;
 
 public class Player {
 
+    //A player's attributes are implemented
+    String name;
+    String color;
+    int position;
+    static DiceCup diceCup = new DiceCup();
+
+    public Player(String name, String color, int position) {
+        this.name = name;
+        this.color = color;
+        this.position = position;
+    }
+
+    public int rollDie() {
+        return diceCup.roll();
+    }
+
+    //Return name, color and position
+    public String getname(){
+        return this.name;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public int getPosition(){
+        return this.position;
+    }
+
+    //Prints a move from the player and updates their position
+    public void movePosition(int move){
+        System.out.println(this.name + " moved " + move + " fields");
+        this.position += move;
+    }
+
 }
