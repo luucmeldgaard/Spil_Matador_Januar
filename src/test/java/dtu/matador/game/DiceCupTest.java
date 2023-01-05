d54bece4eeb2bf739880f030622112f17c7b6686
LuucM <luucmeldgaard@outlook.com>
2023-01-05 17:34:46 +0100

    Added DiceCupTest

---
+++ src/test/java/dtu/matador/game/DiceCupTest.java
@@ -0,0 +1,21 @@
+package dtu.matador.game;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceCupTest {
+
+    @Test
+    public void dieInRangeOneToSix() {
+        DiceCup diceCup = new DiceCup();
+        int result;
+        for (int i = 0; i < 200; i++) {
+            result = diceCup.roll();
+            System.out.println(result);
+            Assert.assertTrue(result >= 2 && result <= 12);
+        }
+
+    }
+}
