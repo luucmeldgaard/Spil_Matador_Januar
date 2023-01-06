b797d1031d3c8014da10ef1fa4b65d3287ac86b3
Haidar Alwandi <Haidar2001@hotmail.dk>
2023-01-06 16:17:56 +0100

    Purchasabe Properties for den abstrakte klasse Property implementer Herunder Railroad, Street og Utility -klasserne.

--- src/main/java/dtu/matador/game/fields/Utility.java
+++ src/main/java/dtu/matador/game/fields/Utility.java
@@ -3,8 +3,23 @@ package dtu.matador.game.fields;
 import dtu.matador.game.Property;
 
 public class Utility extends Property {
-
+    String name;
+    String subtext;
+    String description;
+    String rent;
+    String color_1;
+    String color_2;
+    String price;
+    String owner;
     public Utility(String name, String subtext, String description, String rent, String color_1, String color_2, String price, String owner) {
         super(name, subtext, description, rent, color_1, color_2, price, owner);
+        this.name = name;
+        this.subtext = subtext;
+        this.description = description;
+        this.rent = rent;
+        this.color_1 = color_1;
+        this.color_2 = color_2;
+        this.price = price;
+        this.owner = owner;
     }
 }
