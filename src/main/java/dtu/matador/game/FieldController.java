1751bec822191f10b10d7af539c14e2c42030a50
LuucM <luucmeldgaard@outlook.com>
2023-01-06 20:00:40 +0100

    Cleaned up manual testing for FieldController.java

--- src/main/java/dtu/matador/game/FieldController.java
+++ src/main/java/dtu/matador/game/FieldController.java
@@ -6,8 +6,5 @@ import java.io.IOException;
 
 public class FieldController {
 
-    public static void main(String[] args) throws IOException, ParseException {
-        FieldLoader loader = new FieldLoader("fieldSpaces");
-    }
 
 }
