fbd7acc3108599652aae96d18f079b2c899b8707
Haidar Alwandi <Haidar2001@hotmail.dk>
2023-01-11 20:35:38 +0100

    comment in JSONCreator

--- src/main/java/dtu/matador/game/JSONCreator.java
+++ src/main/java/dtu/matador/game/JSONCreator.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 
 public class JSONCreator {
 
+    //Main method. Runs the program
     public static void main(String[] args) throws IOException {
 
         Scanner scan = new Scanner(System.in);
