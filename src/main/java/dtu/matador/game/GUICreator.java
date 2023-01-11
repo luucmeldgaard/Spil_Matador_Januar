a6a33eee2f5c0dbf877fa7c0cc50a46f70ce30e3
Lars Siegvardsen <s224287@Student.dtu.dk>
2023-01-11 22:48:06 +0100

    Merged branch into development

--- src/main/java/dtu/matador/game/GUICreator.java
+++ src/main/java/dtu/matador/game/GUICreator.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 public class GUICreator {
     public GUICreator() {
-
     }
     //Checks what fieldType is used
     public GUI_Field[] setup(Map<String, Map<String, String>> fieldMap) {
