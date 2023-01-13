15dfdcc1e8fbbdb3da54e56dcc23c8fd8dcbe6e6
LuucM <luucmeldgaard@outlook.com>
2023-01-13 22:25:55 +0100

    Set variables' and methods' visibility in NonPurchasableFields.java

--- src/main/java/dtu/matador/game/NonPurchasableFields.java
+++ src/main/java/dtu/matador/game/NonPurchasableFields.java
@@ -2,13 +2,13 @@ package dtu.matador.game;
 
 //NonPurchasableFields is implemented as an abstract class
 public abstract class NonPurchasableFields implements FieldSpaces {
-    FieldController controller;
-    String name;
-    String subtext;
-    String description;
-    String color1;
-    String color2;
-    int position;
+    private static FieldController controller;
+    private final String name;
+    private final String subtext;
+    private final String description;
+    private final String color1;
+    private final String color2;
+    private final int position;
 
     //NonPurchasableFields has all the parameters that the properties which you cannot own, have in common
     public NonPurchasableFields(FieldController controller, String name, String subtext, String description, String color1, String color2, String position) {
