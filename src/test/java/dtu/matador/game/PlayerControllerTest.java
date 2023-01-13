825a19becba22758a7e93e0f43ade3a5cba1bab7
LuucM <luucmeldgaard@outlook.com>
2023-01-13 21:32:11 +0100

    modified PlayerController Tests

--- src/test/java/dtu/matador/game/PlayerControllerTest.java
+++ src/test/java/dtu/matador/game/PlayerControllerTest.java
@@ -1,49 +1,73 @@
 package dtu.matador.game;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerControllerTest {
-    PlayerController playerController = new PlayerController();
-
-
 
     @Test
-    public void testHandleTransactionPositive() {
+    public void testHandleTransactionPositiveForTarget() {
+        PlayerController playerController = new PlayerController();
         String targetPlayerName = "player1";
         String beneficiaryName = "player2";
-        int amount = 1000;
+        int amount = 3000;
+        boolean critical = false;
+        Player beneficiary = null;
+
+        playerController.addPlayer(targetPlayerName, "myBlue", 5, 5000);
+
+        Player targetPlayer = playerController.getPlayerFromName(targetPlayerName);
+
+        boolean result = playerController.handleTransaction(targetPlayer.getId(), null, amount, critical);
+
+        assertTrue(result);
+        assertEquals(8000, targetPlayer.getBalance());
+    }
+
+    @Test
+    public void testHandleTransactionPayBeneficiary() {
+        PlayerController playerController = new PlayerController();
+        String targetPlayerName = "player1";
+        String beneficiaryName = "player2";
+        int amount = -2000;
         boolean critical = false;
 
-        Player targetPlayer = new Player(targetPlayerName, "myBlue", 5, 5000);
-        Player beneficiary = new Player(beneficiaryName, "myRed", 10, 5000);
+        playerController.addPlayer(targetPlayerName, "myBlue", 5, 5000);
+        playerController.addPlayer(beneficiaryName, "myRed", 5, 5000);
 
-        playerController.players.add(targetPlayer);
-        playerController.players.add(beneficiary);
+        Player targetPlayer = playerController.getPlayerFromName(targetPlayerName);
+        Player beneficiary = playerController.getPlayerFromName(beneficiaryName);
 
         boolean result = playerController.handleTransaction(targetPlayer.getId(), beneficiary.getId(), amount, critical);
 
         assertTrue(result);
-        assertEquals(4000, targetPlayer.getBalance());
-        assertEquals(6000, beneficiary.getBalance());
+        assertEquals(3000, targetPlayer.getBalance());
+        assertEquals(7000, beneficiary.getBalance());
     }
 
     @Test
-    public void testHandleTransactionNegative() {
+    public void testHandleTransactionTargetCantPay() {
+        PlayerController playerController = new PlayerController();
         String targetPlayerName = "player1";
         String beneficiaryName = "player2";
-        int amount = 150;
+        int amount = -12000;
         boolean critical = true;
 
-        Player targetPlayer = new Player(targetPlayerName, "myBlue", 5, 5000);
-        Player beneficiary = new Player(beneficiaryName, "myRed", 10, 5000);
+        playerController.addPlayer(targetPlayerName, "myBlue", 5, 5000);
+        playerController.addPlayer(beneficiaryName, "myRed", 5, 5000);
 
-
+        Player targetPlayer = playerController.getPlayerFromName(targetPlayerName);
+        Player beneficiary = playerController.getPlayerFromName(beneficiaryName);
+        int targetPlayerActualBalance = targetPlayer.getBalance();
 
         boolean result = playerController.handleTransaction(targetPlayer.getId(), beneficiary.getId(), amount, critical);
 
         assertFalse(result);
-        assertEquals(0, targetPlayer.getBalance());
-        assertEquals(150, beneficiary.getBalance());
+        assertNull(playerController.getPlayerFromName(targetPlayerName));
+        assertEquals(5000 + targetPlayerActualBalance, beneficiary.getBalance());
     }
+
 }
