03589a963ef2e4fb3549b9f980f0dc555716e6c8
LuucM <luucmeldgaard@outlook.com>
2023-01-16 14:58:17 +0100

    All default files have been put in the ressources and the FIeldLoader is now ready to become a common Loader class

--- src/main/java/dtu/matador/game/FieldLoader.java
+++ src/main/java/dtu/matador/game/FieldLoader.java
@@ -15,24 +15,23 @@ public class FieldLoader {
     ArrayList<Map<String, String>> chanceList;
 
     public FieldLoader(String selectedBoard) {
-        boardList = JSONtoList(selectedBoard + ".json", "position");
+        boardList = JSONtoList(selectedBoard + ".json", "position", "board");
         this.colorMap = new HashMap<>();
-        this.colorMap = JSONtoMapColors("colors.json");
-        this.chanceList = JSONtoList("chance" + ".json", "Number");
+        this.colorMap = JSONtoMapColors("colors.json", "colors");
+        this.chanceList = JSONtoList("chance" + ".json", "Number", "chance");
     }
 
-    private ArrayList<Map<String, String>> JSONtoList(String filename, String outerMapKey) {
 
-        // The map to return
-        ArrayList<Map<String, String>> board = new ArrayList<>();
+    // A JSON reader which reads a JSON and parses it to a string,
+    // which it then returns.
+    public String read(String filename, String jsonType) {
 
-        // Makes it possible to parse JSON and map JSON text
-        JSONParser jsonParser = new JSONParser();
-        ObjectMapper mapper = new ObjectMapper();
+        // String to return
+        String objString = "";
 
         // Defines the file and retrieves its canonical path
         String path = "";
-        File file = new File("../json/" + filename);
+        File file = new File("./json/" + filename);
         try {
             path = file.getCanonicalPath();
             System.out.println(path);
@@ -43,16 +42,61 @@ public class FieldLoader {
         // Tries to parse the JSON to a Map<String, Map<String>, <String>
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
 
+            // Makes it possible to parse JSON
+            JSONParser jsonParser = new JSONParser();
+
             // Creates an array of strings from an object created by the JsonParser
             // to be able to parse each field to the mapper.
             Object obj = jsonParser.parse(reader);
-            String objString = obj.toString();
-            objString = objString.substring(1, objString.length() - 1);
-            String[] objArray = objString.split("},");
-            for (int i = 0; i < objArray.length; i++) {
-                board.add(null);
-            }
+            objString = obj.toString();
+        }
 
+        // throws an exception
+        // If the file doesn't exist in the canonical path ./json folder,
+        // then it will try to retrieve the default JSON game files which can be found in the ressources.
+        catch (IOException | ParseException ex) {
+            try {
+                switch (jsonType) {
+                    case "board" -> filename = "default_fieldData.json";
+                    case "chance" -> filename = "default_chance.json";
+                    case "colors" -> filename = "default_colors.json";
+                }
+                InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename);
+                assert inputStream != null;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                // Makes it possible to parse JSON
+                JSONParser jsonParser = new JSONParser();
+
+                // Creates an array of strings from an object created by the JsonParser
+                // to be able to parse each field to the mapper.
+                Object obj = jsonParser.parse(reader);
+                objString = obj.toString();
+            }
+            catch (IOException | ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return objString;
+    }
+
+    // Creates an ArrayList of maps from a JSON file
+    private ArrayList<Map<String, String>> JSONtoList(String filename, String outerMapKey, String jsonType) {
+
+        // The map to return
+        ArrayList<Map<String, String>> board = new ArrayList<>();
+
+        // Makes it possible to map JSON text
+        ObjectMapper mapper = new ObjectMapper();
+
+        String objString = read(filename, jsonType);
+        objString = objString.substring(1, objString.length() - 1);
+        String[] objArray = objString.split("},");
+        for (int i = 0; i < objArray.length; i++) {
+            board.add(null);
+        }
+
+        try {
             // Adds each field to the board map
             for (String item : objArray) {
                 item = item + "}";
@@ -60,60 +104,28 @@ public class FieldLoader {
                 int position = Integer.parseInt(map.get(outerMapKey));
                 board.set(position, map);
             }
-            reader.close();
-
-        }
-        //throws an exception
-        catch (IOException | ParseException ex) {
-            try {
-                InputStream inputStream = getClass().getClassLoader().getResourceAsStream("FieldData.json");
-                assert inputStream != null;
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                // Creates an array of strings from an object created by the JsonParser
-                // to be able to parse each field to the mapper.
-                Object obj = jsonParser.parse(reader);
-                String objString = obj.toString();
-                objString = objString.substring(1, objString.length() - 1);
-                String[] objArray = objString.split("},");
-                for (int i = 0; i < objArray.length; i++) {
-                    board.add(null);
-                }
-
-                // Adds each field to the board map
-                for (String item : objArray) {
-                    item = item + "}";
-                    Map<String, String> map = mapper.readValue(item, Map.class);
-                    int position = Integer.parseInt(map.get(outerMapKey));
-                    board.set(position, map);
-                }
-                reader.close();
-            }
-            catch (IOException | ParseException e) {
-                throw new RuntimeException(e);
-            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
         return board;
     }
-    //Converts JSON to colors on map
-    private Map<String, String> JSONtoMapColors(String filename) {
-        JSONParser jsonParser = new JSONParser();
-        try (FileReader fieldFileReader = new FileReader(filename)) {
-            Object obj = jsonParser.parse(fieldFileReader);
 
-            ObjectMapper mapper = new ObjectMapper();
-            String objString = obj.toString();
-            objString = objString.replace("[", "");
-            objString = objString.replace("]", "");
+    //Converts JSON to colors on map
+    private Map<String, String> JSONtoMapColors(String filename, String jsonType) {
+        ObjectMapper mapper = new ObjectMapper();
+        String objString = read(filename, jsonType);
+        objString = objString.replace("[", "");
+        objString = objString.replace("]", "");
+        try{
             Map<String, String> map = mapper.readValue(objString, Map.class);
             for (String color : map.keySet()) {
                 System.setProperty(color, map.get(color));
             }
             return map;
         }
-        catch (IOException | ParseException ex) {
-            throw new RuntimeException(ex);
+        catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
